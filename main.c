faba07c215bd96ec69551e5501ec078c2639149b
Richard Uggelberg <richard.uggelberg@gmail.com>
2016-10-05 21:16:27 +0200

    Basic algorithm that only works halfway through with "stripes" example

--- main.c
+++ main.c
@@ -5,6 +5,60 @@
 
 int main(  void )
 {
-	printf("hello!"); //This is a comment made by Richard
+	//all pixels
+	int in[1024];
+
+	//8 columns in 16 squares
+	//each column is represented by a (?)
+	int out[128];
+
+	int i = 0;
+	int j = 0;
+
+	//Example
+	//Set in as a striped "picture"
+	//Should put out a list of "170"
+	for(i = 0; i < 1024; i++)
+	{
+		if(i%2 == 0)
+		{
+			in[i] = 1;
+		}
+		else 
+		{
+			in[i] = 0;
+		}
+	}
+
+	//out elements used in inner loop.
+	int outElement = 0;
+
+	//Set output array
+	for(i = 0; i < 128; i++)
+	{
+		for(j = 0; j < 8; j++)
+		{
+			outElement += in[(i*16) + j] << (j+1);
+		}
+
+		out[i] = outElement;
+
+		outElement = 0;
+	}
+
+	//Print output array
+	for(i = 0; i < 128; i++)
+	{
+		if((i+1)%8 == 0)
+		{
+			printf("%d, \n", out[i]);
+		}
+		else
+		{
+			printf("%d, ", out[i]);
+		}
+	}
+
+
 	return 1;
 }
